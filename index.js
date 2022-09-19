cd402ba6b5df479c9ac023582a3958b181f242d5
Brock Alberry <vowywowy@gmail.com>
2022-09-19 20:25:31 +0000

    bot

---
+++ index.js
@@ -0,0 +1,36 @@
+const { Client, Intents, MessageAttachment } = require('discord.js');
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const DUDE_PALS_GENERAL = '753082935451582519';
+const SPECIAL_EVENTS = {
+    '1': {
+        attachment: 'https://cdn.discordapp.com/attachments/753082935451582519/889692575504687104/videoplayback-1.mp4',
+        embed: {
+            title: "milf",
+            image: {
+                url: "https://cdn.discordapp.com/attachments/753082935451582519/889692575504687104/videoplayback-1.mp4"
+            }
+        }
+    },
+    '3': {
+        attachment: 'https://i.kym-cdn.com/entries/icons/original/000/020/016/wednesdaymydudeswide.jpg',
+        embed: {
+            title: "wednesday",
+            image: {
+                url: "https://i.kym-cdn.com/entries/icons/original/000/020/016/wednesdaymydudeswide.jpg"
+            }
+        }
+    }
+}
+
+bot.on("ready", () => {
+    DUDE_PALS_GENERAL = bot.channels.cache.get(DUDE_PALS_GENERAL); // Channel to send notification
+});
+
+DUDE_PALS_GENERAL.send({
+    embeds: [SPECIAL_EVENTS[process.env.DAY.toLocaleUpperCase()].embed],
+    files: [new MessageAttachment(SPECIAL_EVENTS[f`${new Date().getDay}`].attachment)]
+});
+
+
+bot.login(process.env.CLIENT_TOKEN);
